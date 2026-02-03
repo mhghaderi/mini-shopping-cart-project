9e5461c1aba01476b47de013c92e3fe956f3d6bc
mmd 2 <mhghaderi@yahoo.com>
2026-02-03 10:27:00 +0330

    upload to github

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import { ShoppingCart, Home } from "lucide-react";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <header
+        className="sticky top-0 bg-gray-950/90 backdrop-blur-md text-white shadow-2xl
+      shadow-gray-950/70 border-b border-orange-900"
+      >
+        <div className="container mx-auto p-4 flex items-center justify-between"></div>
+        <div className="flex items-center space-x-4 cursor-pointer">
+          <Home />
+        </div>
+      </header>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
